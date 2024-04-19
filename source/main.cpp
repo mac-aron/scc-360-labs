7f7c09b5d8426b3873382b7fc027d36031154c4c
mac-aron <aroneggens@gmail.com>
2024-04-19 15:36:16 +0200

    First commit after setting up

--- source/main.cpp
+++ source/main.cpp
@@ -1,14 +1,11 @@
 #include "MicroBit.h"
-#include "samples/Tests.h"
-
-MicroBit uBit;
+NRF52Pin usbTx(ID_PIN_USBTX, MICROBIT_PIN_UART_TX, PIN_CAPABILITY_DIGITAL);
+NRF52Pin usbRx(ID_PIN_USBRX, MICROBIT_PIN_UART_RX, PIN_CAPABILITY_DIGITAL);
+NRF52Serial serial(usbTx, usbRx, NRF_UARTE0);
 
 int main()
 {
-    uBit.init();
-
-    out_of_box_experience();
-
-    microbit_panic( 999 );
+    int something = 45;
+    serial.printf("Hello World!\n");
+    serial.printf(" something: %d\n", something);
 }
-
