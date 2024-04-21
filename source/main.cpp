1cd47b55329bc95e07382f846abc0ea9e011aff9
mac-aron <aroneggens@gmail.com>
2024-04-22 00:37:03 +0200

    Added serial debugging imports

--- source/main.cpp
+++ source/main.cpp
@@ -1,3 +1,9 @@
+// Library imports for debugging only
+#include "MicroBit.h"
+NRF52Pin usbTx(ID_PIN_USBTX, MICROBIT_PIN_UART_TX, PIN_CAPABILITY_DIGITAL);
+NRF52Pin usbRx(ID_PIN_USBRX, MICROBIT_PIN_UART_RX, PIN_CAPABILITY_DIGITAL);
+NRF52Serial serial(usbTx, usbRx, NRF_UARTE0);
+
 int main() {
     while(true);
 }
